e36faea8c4c2ebca88353c64ca421e369b9e7cef
burcukarabina <burcutu@gmail.com>
2026-07-15 12:34:07 -0400

    Update matrixCalculator.js

--- calculators/matrixCalculator.js
+++ calculators/matrixCalculator.js
@@ -1,1 +1,12 @@
-alert("Matrix Calculator loaded!");
+console.log("Matrix Calculator JS loaded.");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const p = document.getElementById("externalJsTest");
+
+    if (p) {
+        p.innerHTML =
+            "<span style='color:green;font-weight:bold;'>✓ External JavaScript loaded successfully!</span>";
+    }
+
+});
