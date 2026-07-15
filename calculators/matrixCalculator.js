e6e7f43e980021e828a57c25689d0582d77882a1
burcukarabina <burcutu@gmail.com>
2026-07-15 14:30:31 -0400

    Update matrixCalculator.js

--- calculators/matrixCalculator.js
+++ calculators/matrixCalculator.js
@@ -218,7 +218,7 @@
     link.id = CSS_ID;
     link.rel = "stylesheet";
     link.href =
-  "https://burcukarabina.github.io/computational-math/css/calculators.css?v=5";
+  "https://burcukarabina.github.io/computational-math/css/calculators.css?v=6";
 
     document.head.appendChild(link);
   }
